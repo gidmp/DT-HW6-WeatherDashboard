0e66427f6ebbda7528df692aaa691f4fe4bb8478
Daniel Luke Tanoeihusada <danielluke08@gmail.com>
2020-04-08 14:48:24 -0700

    added forecast and local storage functions

--- script.js
+++ script.js
@@ -2,61 +2,139 @@
 
 $(document).ready(function() {
     var cityHistory = [];
-
     //when search button is clicked
     $("#search-city-button").on("click", function(event) {
         event.preventDefault();
         // This line grabs the input from the textbox
         var city = $("#city-input").val().trim();
-
         // Adding cities from the textbox to array
         cityHistory.push(city);
-
         //immediately take the value and run it on ajax
-
-        // Calling renderButtons which handles the processing of our movie array
+        displayCurrentInfo(city);
+        displayFutureInfo(city);
         renderButtons();
+        //store the value of the last searched city in local storage
+        var cityName = $("#city-input").val();
+        storeInput(cityName);
         //clear input form after submission
         $("#city-input").val("");
-
     });
-    
+   
     //render city history buttons
     function renderButtons(){
         $("#search-history").empty();
-
         for (var i = 0; i < cityHistory.length; i++) {
             var createButtons = $("<button>");
             //add class to each buttons
             createButtons.addClass("cityButtons");
-            //add attrinute equals to the city names
             createButtons.attr("data-name", cityHistory[i]);
-            //print the city name on the buttons
             createButtons.text(cityHistory[i]);
             //prepend the city name into the div
             $("#search-history").prepend(createButtons);
         }
     }
-
-
-    function displayInfo(){
-        var cityName = $(this).attr("data-name");
+    function displayCurrentInfo(cityName){
         var api = "96428242b049309d31d51b7cb823fae0";
-        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ cityName +"&appid=" + api;
-
-        
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&appid=" + api;
+       
         $.ajax({
         url: queryURL,
         method: "GET"
         }).then(function(response) {
             console.log(response);
             //write the code to create and append the info on screen
+            //add current date with moment JS after the city name
+            $(".city").text(response.name);  
+            $(".temperature").text("Temperature:  " + response.main.temp + "K");  
+            $(".humidity").text("Humidity:  "+ response.main.humidity + "%");
+            $(".wind-speed").text("Wind speed:  "+ response.wind.speed + "MPH");
+            //Uv-index safety
+           
+            //get city coordinate
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+  
+            //function to findout the UV index of city based on its coordinate using closure
+            var uvIndex = function(lat, lon){
+                var UVqueryURL = "http://api.openweathermap.org/data/2.5/uvi?appid="+ api +"&lat="+ lat +"&lon="+ lon;
+                $.ajax({
+                    url: UVqueryURL,
+                    method: "GET"
+                    }).then(function(response) {
+                        console.log(response)
+  
+                        var uvValue = response.value;
+                        var uvSafety = $("<span>").attr("id", "uvSafety").addClass("badge");
+                        uvSafety.text(uvValue);
+  
+                        if (uvValue <= 2){
+                            uvSafety.css("background-color", "lightgreen");
+                        }else if (uvValue >=3 && uvValue <=7){
+                            uvSafety.css("background-color", "yellow");
+                        }else{
+                            uvSafety.css("background-color", "red");
+                        }
+  
+                        $(".uv-index").text("UV Index:  " );
+                        $(".uv-index").append(uvSafety);
+  
+                    })
+            }
+  
+            //call uvIndex function using closure
+            uvIndex(lat,lon);
+           
         })
     }
-
+    function displayFutureInfo(cityName){
+        var api = "96428242b049309d31d51b7cb823fae0";
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ cityName +"&appid=" + api;
+       
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(response) {
+            //empty out the previous elements
+            $("#forecast").empty();
+            console.log(response);
+            var index = [3,11,19,27,35];
+ 
+            for(var i = 0; i < index.length; i++){
+                //look at working movie app activity for this
+                var createDiv = $("<div class = 'card'>");
+                var lineBreak = $("<br>");
+ 
+                var temp = response.list[index[i]].main.temp;
+                var pTemp = $("<p>").text("Temp: " + temp);
+                createDiv.append(pTemp, lineBreak);
+ 
+                var humidity = response.list[index[i]].main.humidity;
+                var pHumid = $("<p>").text("Humidity: " + humidity);
+                createDiv.append(pHumid);
+ 
+                
+                $("#forecast").append(createDiv);
+            }
+  
+           
+           
+        })
+    }
+ 
+    function storeInput(cityName){
+        localStorage.setItem("Last city searched", cityName);
+    }
+    displayCurrentInfo(localStorage.getItem("Last city searched"));
+    displayFutureInfo(localStorage.getItem("Last city searched"));
+ 
+   
+ 
     //when city history button is click, run through displayInfo function and print the info
-    $(document).on("click", ".cityButtons", displayInfo);
-
-
-
-})
+    $(document).on("click", ".cityButtons", function(event) {
+        console.log("hi");
+        event.preventDefault();
+        var cityName = $(this).attr("data-name");
+        displayCurrentInfo(cityName);
+        displayFutureInfo(cityName);
+    })
+ })
